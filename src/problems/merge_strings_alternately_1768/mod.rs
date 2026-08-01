e4dbd4d31a9fbc541e41543a42b47c3446d55ca5
shafqat1841 <36793814+shafqat1841@users.noreply.github.com>
2026-08-01 10:00:54 +0000

    feat: write solution for merge_strings_alternately_1768

--- src/problems/merge_strings_alternately_1768/mod.rs
+++ src/problems/merge_strings_alternately_1768/mod.rs
@@ -1,8 +1,34 @@
+#[allow(dead_code)]
 struct Solution;
 
 impl Solution {
+    #[allow(dead_code)]
     pub fn merge_alternately(word1: String, word2: String) -> String {
-        "".to_string()
+        let word1_bytes = word1.into_bytes();
+        let word2_bytes = word2.into_bytes();
+
+        let word1_length = word1_bytes.len();
+        let word2_length = word2_bytes.len();
+        
+        let word3_length = word1_length + word2_length;
+        
+        let mut word3 = Vec::<u8>::new();
+
+        for i in 0..word3_length {
+            let word1_char = word1_bytes.get(i);
+            let word2_char = word2_bytes.get(i);
+            if let Some(v) = word1_char {
+                word3.push(*v);
+            }
+            if let Some(v) = word2_char {
+                word3.push(*v);
+            }
+        }
+
+        match String::from_utf8(word3) {
+            Ok(string) => string,
+            Err(e) => format!("Invalid UTF-8 bytes: {}", e),
+        }
     }
 }
 
@@ -11,8 +37,26 @@ mod merge_strings_alternately_1768_test {
     use super::*;
 
     #[test]
-    fn it_works() {
-       let result = Solution::merge_alternately( "".to_string(), "".to_string());
-        assert_eq!(result, "");
+    fn check_no_1() {
+        let result = Solution::merge_alternately("abc".to_string(), "def".to_string());
+        assert_eq!(result, "adbecf");
+    }
+
+    #[test]
+    fn check_no_2() {
+        let result = Solution::merge_alternately("abc".to_string(), "pqr".to_string());
+        assert_eq!(result, "apbqcr");
+    }
+
+    #[test]
+    fn check_no_3() {
+        let result = Solution::merge_alternately("ab".to_string(), "pqrs".to_string());
+        assert_eq!(result, "apbqrs");
+    }
+
+    #[test]
+    fn check_no_4() {
+        let result = Solution::merge_alternately("abcd".to_string(), "pq".to_string());
+        assert_eq!(result, "apbqcd");
     }
 }
