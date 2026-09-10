b1b8b3e83c143f6d46717cae528be22bc9a3d243
shafqat1841 <36793814+shafqat1841@users.noreply.github.com>
2026-09-10 14:31:38 +0000

    fix: fix bug and optimize algorithem

--- src/sorting/quick_sort/mod.rs
+++ src/sorting/quick_sort/mod.rs
@@ -3,9 +3,10 @@ struct Solution;
 fn partition(arr: &mut [i32], low: usize, high: usize) -> usize {
     // println!("arr 1: {:?}", arr);
     let pivot_value = arr[low];
-    let mut left_low = low;
+    let mut left_low = low + 1;
 
     for i in low + 1..high {
+        // println!("arr loop: {:?}", arr);
         if arr[i] < pivot_value {
             let temp = arr[i];
             arr[i] = arr[left_low];
@@ -15,28 +16,57 @@ fn partition(arr: &mut [i32], low: usize, high: usize) -> usize {
         }
     }
 
-    arr[left_low] = pivot_value;
+    // println!("arr 1.5: {:?}", arr);
+    let temp_left_low = arr[left_low - 1];
+    arr[left_low - 1] = pivot_value;
+    arr[0] = temp_left_low;
+
     // println!("arr 2: {:?}", arr);
 
-    // println!("left_low: {:?}", left_low);
-    left_low
+    left_low - 1
 }
 
-fn quick_sort(arr: &mut [i32]){
+fn quick_sort(arr: &mut [i32]) {
     let low: usize = 0;
     let high: usize = arr.len();
 
     if low < high {
         let pivot = partition(arr, low, high);
+        println!("pivot: {:?}", pivot);
         quick_sort(&mut arr[low..pivot]);
         quick_sort(&mut arr[pivot + 1..high]);
-        println!("arr 4: {:?}", arr);
     }
-
 }
 
 impl Solution {
     fn sort(arr: &mut [i32]) {
+        let low: usize = 0;
+        let high: usize = arr.len();
+
+        let mid = (high - 1) / 2;
+
+        if arr[low] > arr[mid] {
+            let temp = arr[low];
+            arr[low] = arr[mid];
+            arr[mid] = temp;
+        }
+
+        if arr[low] > arr[high - 1] {
+            let temp = arr[low];
+            arr[low] = arr[high - 1];
+            arr[high - 1] = temp;
+        }
+
+        if arr[mid] > arr[high - 1] {
+            let temp = arr[mid];
+            arr[mid] = arr[high - 1];
+            arr[high - 1] = temp;
+        }
+
+        let temp = arr[low];
+        arr[low] = arr[mid];
+        arr[mid] = temp;
+
         quick_sort(arr);
     }
 }
@@ -46,7 +76,7 @@ mod quick_sort_test {
     use super::*;
 
     #[test]
-    fn check_solution() {
+    fn test_1() {
         let mut unsorted_arr = [30, 27, 25, 20, 18, 17, 10, 5];
         let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27, 30];
         Solution::sort(&mut unsorted_arr);
@@ -54,15 +84,15 @@ mod quick_sort_test {
     }
 
     #[test]
-    fn check_solution_2() {
-        let mut unsorted_arr = [27, 25, 20, 18, 17, 10, 5];
-        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27];
+    fn test_2() {
+        let mut unsorted_arr = [30, 27, 25, 20, 18, 17, 10, 5];
+        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27, 30];
         Solution::sort(&mut unsorted_arr);
         assert_eq!(unsorted_arr, sorted_arr_res);
     }
 
     #[test]
-    fn check_solution_3() {
+    fn test_3() {
         let mut unsorted_arr = [30, 27, 18, 17, 10, 5, 25, 20];
         let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27, 30];
         Solution::sort(&mut unsorted_arr);
@@ -70,7 +100,7 @@ mod quick_sort_test {
     }
 
     #[test]
-    fn check_solution_4() {
+    fn test_4() {
         let mut unsorted_arr = [5, 10, 17, 18, 30, 27, 25, 20];
         let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27, 30];
         Solution::sort(&mut unsorted_arr);
@@ -78,25 +108,33 @@ mod quick_sort_test {
     }
 
     #[test]
-    fn check_solution_5() {
-        let mut unsorted_arr = [27, 25, 17, 10, 5, 20, 18];
-        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27];
+    fn test_5() {
+        let mut unsorted_arr = [30, 27, 25, 17, 10, 5, 20, 18];
+        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27, 30];
         Solution::sort(&mut unsorted_arr);
         assert_eq!(unsorted_arr, sorted_arr_res);
     }
 
     #[test]
-    fn check_solution_6() {
-        let mut unsorted_arr = [17, 10, 5, 27, 25, 20, 18];
-        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27];
+    fn test_6() {
+        let mut unsorted_arr = [17, 10, 5, 30, 27, 25, 20, 18];
+        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27, 30];
         Solution::sort(&mut unsorted_arr);
         assert_eq!(unsorted_arr, sorted_arr_res);
     }
 
     #[test]
-    fn check_solution_7() {
-        let mut unsorted_arr = [5, 10, 17, 27, 25, 20, 18];
-        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27];
+    fn test_7() {
+        let mut unsorted_arr = [5, 10, 17, 30, 27, 25, 20, 18];
+        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27, 30];
+        Solution::sort(&mut unsorted_arr);
+        assert_eq!(unsorted_arr, sorted_arr_res);
+    }
+
+    #[test]
+    fn test_8() {
+        let mut unsorted_arr = [20, 10, 17, 30, 27, 25, 5, 18];
+        let sorted_arr_res = [5, 10, 17, 18, 20, 25, 27, 30];
         Solution::sort(&mut unsorted_arr);
         assert_eq!(unsorted_arr, sorted_arr_res);
     }
