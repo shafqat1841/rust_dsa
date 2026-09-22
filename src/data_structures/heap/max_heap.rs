6ea1abecc84c7d547e539e5d92f2f4e99b814f50
shafqat1841 <36793814+shafqat1841@users.noreply.github.com>
2026-09-22 09:26:15 +0000

    feat: add helper function to get node

--- src/data_structures/heap/max_heap.rs
+++ src/data_structures/heap/max_heap.rs
@@ -1,18 +1,28 @@
 struct solution;
 
 impl solution {
-    fn create(arr: [i32; 10]) {
-        
+    fn create(arr: [i32; 10]) {}
+
+    fn get_left_child(index: usize) -> usize {
+        index * 2
+    }
+
+    fn get_right_child(index: usize) -> usize {
+        index * 2 + 1
+    }
+
+    fn get_parent(index: usize) -> usize {
+        index / 2
     }
 }
 
-#[cfg(test)] 
+#[cfg(test)]
 mod max_heap_test {
     use super::*;
 
     #[test]
     fn create_data_structure() {
-        let array = [1,2,3,4,5,6,7,8,9,10];
+        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         solution::create(array);
     }
 }
